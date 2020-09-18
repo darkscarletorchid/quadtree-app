8f06601e20dd2c558b732130de96f00c39d29ee7
A T <dark.scarlet.orhid@gmail.com>
2020-09-18 20:51:29 +0300

    Updates

---
+++ precache-manifest.a538a4d1939653476330d72317bfb7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be9cbc0844f591628ba31f0e889a711",
+    "url": "/quadtree/index.html"
+  },
+  {
+    "revision": "3f1f76036568fd7bdfb9",
+    "url": "/quadtree/static/css/main.cdf6e972.chunk.css"
+  },
+  {
+    "revision": "6cf1c6a4406bef8e52fb",
+    "url": "/quadtree/static/js/2.7b2780c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/quadtree/static/js/2.7b2780c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1f76036568fd7bdfb9",
+    "url": "/quadtree/static/js/main.2068d169.chunk.js"
+  },
+  {
+    "revision": "061ad9ec304bbead6453",
+    "url": "/quadtree/static/js/runtime-main.429ff2ed.js"
+  }
+]);
